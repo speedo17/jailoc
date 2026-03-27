e5cd73bd19538c518119ff4e79b4500f0de48bd2
Josef Kolář <josef.kolar@firma.seznam.cz>
2026-03-27 13:27:02 +0100

    fix(version): use debug.ReadBuildInfo as fallback for go install (#40)
    * fix(version): use debug.ReadBuildInfo as fallback for go install
    
    * fix(version): append -dirty suffix when source tree has local modifications
    
    * fix(version): accept vcs.revision shorter than 12 characters

--- cmd/jailoc/main.go
+++ cmd/jailoc/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
 
 	"github.com/seznam/jailoc/internal/cmd"
 )
@@ -13,6 +14,35 @@ var (
 )
 
 func main() {
+	// When installed via "go install", ldflags are not set and version stays "dev".
+	// Fall back to build info embedded by the Go toolchain since 1.18.
+	// See https://pkg.go.dev/runtime/debug#ReadBuildInfo
+	if version == "dev" {
+		if bi, ok := debug.ReadBuildInfo(); ok {
+			if bi.Main.Version != "" && bi.Main.Version != "(devel)" {
+				version = bi.Main.Version
+			}
+			var modified bool
+			for _, s := range bi.Settings {
+				switch s.Key {
+				case "vcs.revision":
+					if len(s.Value) > 12 {
+						commit = s.Value[:12]
+					} else if s.Value != "" {
+						commit = s.Value
+					}
+				case "vcs.time":
+					date = s.Value
+				case "vcs.modified":
+					modified = s.Value == "true"
+				}
+			}
+			if modified {
+				commit += "-dirty"
+			}
+		}
+	}
+
 	if err := cmd.Execute(version, commit, date); err != nil {
 		os.Exit(1)
 	}
